520671ef397d360fef84aad296a43e638efdbd75
Maurice Wijnia <maurice@maurice-macbook.local>
2018-10-19 09:53:12 +0200

    Initial Commit

---
+++ 568225Tests/_68225Tests.swift
@@ -0,0 +1,34 @@
+//
+//  _68225Tests.swift
+//  568225Tests
+//
+//  Created by Maurice Wijnia on 19/10/2018.
+//  Copyright © 2018 Maurice Wijnia. All rights reserved.
+//
+
+import XCTest
+@testable import net_nl_568225
+
+class _68225Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
